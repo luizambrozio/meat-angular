f3e0ef0cc182ad9c7d7405c1d17f8afec89f41cb
Unknown <oliveira.ambrozio@gmail.com>
2017-05-27 19:15:41 -0300

    pagina dos restaurantes pronto

--- src/app/restaurant-detail/shapping-cart/shapping-cart.component.ts
+++ src/app/restaurant-detail/shapping-cart/shapping-cart.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mt-shapping-cart',
-  templateUrl: './shapping-cart.component.html',
-  styleUrls: ['./shapping-cart.component.css']
+  templateUrl: './shapping-cart.component.html'
 })
 export class ShappingCartComponent implements OnInit {
 
