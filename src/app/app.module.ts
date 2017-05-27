60c38b884b4b390053157aecad05013f562bb907
Unknown <oliveira.ambrozio@gmail.com>
2017-05-27 18:54:37 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,39 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-
-import { AppComponent } from './app.component'
+import { ROUTES } from './app.routes'
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ShappingCartComponent } from './restaurant-detail/shapping-cart/shapping-cart.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShappingCartComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [RestaurantsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/restaurant-detail/shapping-cart/shapping-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-shapping-cart',
+  templateUrl: './shapping-cart.component.html',
+  styleUrls: ['./shapping-cart.component.css']
+})
+export class ShappingCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
